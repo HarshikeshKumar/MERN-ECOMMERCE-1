2d4859243479011079f3f69661e0f1d3ae79fca6
HarshikeshKumar <harshikeshkoili214@gmail.com>
2026-03-03 00:21:22 +0530

    SIGNUP Done From Backend

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
 
@@ -9,6 +10,10 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.text());
+
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("API Is Running..........");
