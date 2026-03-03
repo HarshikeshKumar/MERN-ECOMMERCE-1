70b86a713119574b7238b26fc1f892cd40fdda16
HarshikeshKumar <harshikeshkoili214@gmail.com>
2026-03-03 12:12:01 +0530

    LOGIN USER Done With JWT TOKEN from BACKEND Side

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,8 @@
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
+// SignUp USER...............................
 export const signupUser = async (req, res) => {
   try {
     const { name, email, password } = req.body;
@@ -21,6 +23,48 @@ export const signupUser = async (req, res) => {
 
     res.json({
       message: "User resistered successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Server error",
+      error,
+    });
+  }
+};
+
+// Login USER...............................
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({
+        message: "User not found",
+      });
+    }
+
+    // Compare password.....
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) {
+      return res.status(400).json({
+        message: "Invalid credentials",
+      });
+    }
+
+    // Ganerete jwt token
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+    res.json({
+      message: "Login successful",
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
     });
   } catch (error) {
     res.status(500).json({
